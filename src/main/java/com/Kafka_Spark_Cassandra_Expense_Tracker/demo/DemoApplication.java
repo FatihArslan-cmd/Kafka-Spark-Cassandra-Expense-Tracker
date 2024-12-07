0025bd556ba120626b81e413e0c59442dcfb61d8
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-12-07 16:14:21 +0300

    first commit

---
+++ src/main/java/com/Kafka_Spark_Cassandra_Expense_Tracker/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.Kafka_Spark_Cassandra_Expense_Tracker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kafka_Spark_Cassandra_Expense_Tracker/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kafka_Spark_Cassandra_Expense_Tracker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
